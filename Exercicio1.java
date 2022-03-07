c4e53901ad34c9ee568df2af112cd9e369570e18
juAlberti <julia.maia@acad.pucrs.br>
2022-03-07 20:40:26 -0300

    adicionando exercicio 1, precisa testar

---
+++ Exercicio1.java
@@ -0,0 +1,17 @@
+public class Exercicio1 {
+     
+    static int factorialWithRecursion(int n){    
+    if (n == 0)    
+        return 1;    
+    else    
+        return(n * factorialWithRecursion(n-1));    
+    }
+    
+    public static void main(String args[]){  
+        int i, x = 1;  
+        int numberCalculate = 6;
+        fact = factorial(number);   
+        System.out.println("Factorial of "+number+" is: "+fact);    
+        }  
+    
+}
